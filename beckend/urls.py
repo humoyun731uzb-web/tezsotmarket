8853156998ce8f8d55caefb6dd19f8a18fadde03
humoyun731uzb-web <humoyun731uzb@gmail.com>
2026-06-28 11:45:00 +0500

    fix: PWA meta tags moved to head, fixed SW URL, improved media serving

--- beckend/urls.py
+++ beckend/urls.py
@@ -6,10 +6,19 @@ from django.shortcuts import render
 import os
 
 
+import mimetypes
+
 def media_serve(request, path):
+    import os.path
     file_path = os.path.join(settings.MEDIA_ROOT, path)
     if os.path.exists(file_path) and os.path.isfile(file_path):
-        return FileResponse(open(file_path, 'rb'))
+        content_type, _ = mimetypes.guess_type(file_path)
+        if not content_type:
+            content_type = 'application/octet-stream'
+        response = FileResponse(open(file_path, 'rb'), content_type=content_type)
+        response['Content-Length'] = os.path.getsize(file_path)
+        response['Cache-Control'] = 'public, max-age=86400'
+        return response
     raise Http404("Media file not found")
 
 # 404 sahifa
